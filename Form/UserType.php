24fab9d5dd85d0002de7f9c7e713e47cdad58bb4
Valerie <valerie.liakh@devit-team.com>
2021-10-04 04:07:59 +0000

    Agregado CrudBundle como submodule #37

--- Form/UserType.php
+++ Form/UserType.php
@@ -3,6 +3,8 @@ namespace Lynx\ApiBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class UserType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder        ->add('name', TextType::class, array('label' => 'Nombre'))
@@ -11,8 +13,9 @@ class UserType extends AbstractType {
                         ->add('password', TextType::class, array('label' => 'Contraseña',
                             'attr' => array('maxlength' => '8', 'help' => 'Máximo 8 carácteres')))
                         ->add('email', TextType::class, array('label' => 'Correo'))
-                        ->add('isActive', TextType::class, array('choices' => array('Desactivado' => '0', 'Activado' => '1'),
+                        ->add('isActive', ChoiceType::class, array('choices' => array('Desactivado' => '0', 'Activado' => '1'),
                             'label' => 'Estado'))
+                        ->add('rol', TextType::class,array('required' => true, 'label' => 'Tipo'))
         ;
     }
     public function configureOptions(OptionsResolver $resolver) {
